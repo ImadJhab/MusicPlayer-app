cb064a6d7d74145f86a2f815222c18d9d168ec47
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2024-07-02 20:30:15 +0100

    done

--- src/Context.js
+++ src/Context.js
@@ -1,13 +1,16 @@
 import { createContext, useState } from "react";
-
+// Create context for managing music-related state
 export const MusicContext = createContext();
 
+// Context provider to wrap the entire application
 export const ContextProvider = ({ children }) => {
+  // State variables
   const [isLoading, setIsLoading] = useState(false);
   const [likedMusic, setLikedMusic] = useState([]);
   const [pinnedMusic, setPinnedMusic] = useState([]);
   const [resultOffset, setResultOffset] = useState(0);
 
+  // Provide state values and setter functions to the context
   return (
     <MusicContext.Provider
       value={{
